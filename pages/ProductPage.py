c5426ad5d55192abd5c02734d48b95e579e8daca
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-04-04 14:29:36 +0200

    Added access_to_cart method on ProductPage.py

--- pages/ProductPage.py
+++ pages/ProductPage.py
@@ -6,7 +6,9 @@ class ProductPage:
         self.page = page
         self.add_to_cart_button = page.locator("#add-to-cart-sauce-labs-backpack")
         self.get_element_cart = page.locator("span[class='shopping_cart_badge']")
+        self.cart_button = page.locator("a[class*='shopping']")
         self.remove_product_button = page.locator("#remove-sauce-labs-backpack")
+        self.cart_label = page.locator("span[class='title']")
 
     def add_to_cart(self):
         self.add_to_cart_button.click()
@@ -18,6 +20,11 @@ class ProductPage:
         expect(self.get_element_cart).to_have_text("1")
         get_number_article = self.get_element_cart.text_content()
         print(f"The badge text of the article is: {get_number_article}")
+
+    def access_to_cart(self):
+        self.cart_button.click()
+        expect(self.cart_label).to_have_text("Your cart")
+
 
 
 
