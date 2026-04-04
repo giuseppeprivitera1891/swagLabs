05c9d856b0f121038664ede251ddbe59401b7740
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-04-04 14:20:44 +0200

    Added check_status_product_butto methon on ProductPage.py and on testSuite.py

--- pages/ProductPage.py
+++ pages/ProductPage.py
@@ -6,11 +6,21 @@ class ProductPage:
         self.page = page
         self.add_to_cart_button = page.locator("#add-to-cart-sauce-labs-backpack")
         self.get_element_cart = page.locator("span[class='shopping_cart_badge']")
+        self.remove_product_button = page.locator("#remove-sauce-labs-backpack")
 
     def add_to_cart(self):
-        self.add_to_cart_button.first.click()
+        self.add_to_cart_button.click()
         expect(self.get_element_cart).to_have_text("1")
         get_number_article = self.get_element_cart.text_content()
         print(f"The badge text of the article is: {get_number_article}")
+
+    def check_status_product_button(self):
+        expect(self.remove_product_button).to_have_text("Remove")
+        get_remove_text_button = self.remove_product_button.text_content()
+        print(f"The text of the button is: {get_remove_text_button}")
+
+
+
+
 
 
