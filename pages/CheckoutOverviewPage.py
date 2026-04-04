ce22b89e8ef38b04e737fcdcadfa14136c1c57fe
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-04-04 16:45:32 +0200

    Added locators on CheckoutOverviewPage.py

--- pages/CheckoutOverviewPage.py
+++ pages/CheckoutOverviewPage.py
@@ -6,3 +6,15 @@ class CheckoutOverviewPage:
         print("CheckoutOverviewPage")
         self.page = page
 
+        # locators
+        self.payment_info = page.locator("div[data-test='payment-info-label']")
+        self.payment_value = page.locator("div[data-test='payment-info-value']")
+        self.shipping_info = page.locator("div[data-test='shipping-info-label']")
+        self.shipping_value = page.locator("div[data-test='shipping-info-value']")
+        self.price_info = page.locator("div[data-test='total-info-label']")
+        self.subtotal_label = page.locator("div[data-test='subtotal-label']")
+        self.tax_label = page.locator("div[data-test='tax-label']")
+        self.total_label = page.locator("div[data-test='total-label']")
+        self.cancel_button = page.get_by_role("button", name="Cancel")
+        self.finish_button = page.get_by_role("button", name="Finish")
+
