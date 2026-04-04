d27e3121a4bedded3232bc9701ec1195216d3721
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-04-04 15:40:31 +0200

    Added other locators and variables on CheckoutPage.py

--- pages/CheckoutPage.py
+++ pages/CheckoutPage.py
@@ -10,11 +10,15 @@ class CheckoutPage:
         self.first_name = page.get_by_placeholder("First Name")
         self.last_name = page.get_by_placeholder("Last Name")
         self.zip_code = page.get_by_placeholder("Zip/Postal Code")
+        self.cancel_button = page.get_by_role("button", name="Cancel")
+        self.continue_button = page.get_by_role("button", name="Continue")
 
         # variables
         self.first_name_text = "Andrew"
         self.last_name_text = "Brown"
         self.zip_code_text = "12345"
+        self.cancel_button_text = "Cancel"
+        self.continue_button_text = "Continue"
 
 
 
