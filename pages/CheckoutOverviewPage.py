af579a19712631e02b7ef2761d31d6ccbdd86fa3
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-04-04 16:49:05 +0200

    Added variables on CheckoutOverviewPage.py

--- pages/CheckoutOverviewPage.py
+++ pages/CheckoutOverviewPage.py
@@ -18,3 +18,14 @@ class CheckoutOverviewPage:
         self.cancel_button = page.get_by_role("button", name="Cancel")
         self.finish_button = page.get_by_role("button", name="Finish")
 
+        # variables
+        payment_info_text = "Payment Information:"
+        payment_value_text = "SauceCard #31337"
+        shipping_info_text = "Shipping Information:"
+        shipping_value_text = "Free Pony Express Delivery!"
+        price_info_text = "Price Total"
+        subtotal_label_text = "Item total: $29.99"
+        tax_label_text = "Tax: $2.40"
+        total_label_text = "Total: $32.39"
+        cancel_button_text = "Cancel"
+        finish_button_text = "Finish"
