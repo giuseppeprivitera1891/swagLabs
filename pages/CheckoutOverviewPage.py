7e9b0e36bd21f853905da402abce749c06dfb0fb
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-04-04 17:35:56 +0200

    Added check_labels_and_buttons on CheckoutOverviewPage.py

--- pages/CheckoutOverviewPage.py
+++ pages/CheckoutOverviewPage.py
@@ -22,7 +22,7 @@ class CheckoutOverviewPage:
         self.payment_info_text = "Payment Information:"
         self.payment_value_text = "SauceCard #31337"
         self.shipping_info_text = "Shipping Information:"
-        self.shipping_value_textshipping_value_text = "Free Pony Express Delivery!"
+        self.shipping_value_text = "Free Pony Express Delivery!"
         self.price_info_text = "Price Total"
         self.subtotal_label_text = "Item total: $29.99"
         self.tax_label_text = "Tax: $2.40"
@@ -35,3 +35,39 @@ class CheckoutOverviewPage:
         expect(self.payment_info).to_have_text(self.payment_info_text)
         get_payment_info_text = self.payment_info.text_content()
         print(f'Payment Information label is: {get_payment_info_text}')
+        # checks if the detail of the payment information is correct
+        expect(self.payment_value).to_have_text(self.payment_value_text)
+        get_payment_value_text = self.payment_value.text_content()
+        print(f'Payment Value label is: {get_payment_value_text}')
+        # checks if the shipping information label is correct
+        expect(self.shipping_info).to_have_text(self.shipping_info_text)
+        get_shipping_info_text = self.shipping_info.text_content()
+        print(f'Shipping Info label is: {get_shipping_info_text}')
+        # checks if the shipping information value is  correct
+        expect(self.shipping_value).to_have_text(self.shipping_value_text)
+        get_shipping_value_text = self.shipping_value.text_content()
+        print(f'Shipping Value label is: {get_shipping_value_text}')
+        # checks if the price total label is correct
+        expect(self.price_info).to_have_text(self.price_info_text)
+        get_price_info_text = self.price_info.text_content()
+        print(f'Price Info label is: {get_price_info_text}')
+        # checks if the item total value is correct
+        expect(self.subtotal_label).to_have_text(self.subtotal_label_text)
+        get_subtotal_label_text = self.subtotal_label.text_content()
+        print(f'Subtotal label is: {get_subtotal_label_text}')
+        # checks if the tax label is correct
+        expect(self.tax_label).to_have_text(self.tax_label_text)
+        get_tax_label_text = self.tax_label.text_content()
+        print(f'Tax label is: {get_tax_label_text}')
+        # checks if the total label is correct
+        expect(self.total_label).to_have_text(self.total_label_text)
+        get_total_label_text = self.total_label.text_content()
+        print(f'Total label is: {get_total_label_text}')
+        # checks if the cancel text button is correct
+        expect(self.cancel_button).to_have_text(self.cancel_button_text)
+        get_cancel_button_text = self.cancel_button.text_content()
+        print(f'Cancel button is: {get_cancel_button_text}')
+        # checks if the finish text button is correct
+        expect(self.finish_button).to_have_text(self.finish_button_text)
+        get_finish_button_text = self.finish_button.text_content()
+        print(f'Finish button is: {get_finish_button_text}')
