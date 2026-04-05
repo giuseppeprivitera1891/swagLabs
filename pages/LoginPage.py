26ceedf9b8860a52dd9fda68cf4b7bad06365fa6
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-04-05 10:18:26 +0200

    Added locator and variable for product label and check_login_access on LoginPage.py

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 from pytest_playwright.pytest_playwright import page
 
 class LoginPage:
@@ -10,10 +10,19 @@ class LoginPage:
         self.username_textField = page.get_by_placeholder("Username")
         self.password_textField = page.get_by_placeholder("Password")
         self.login_button = page.get_by_role("button", name="Login")
+        self.product_label = page.locator("span[class='title'")
+
+        # variables
+        self.product_label_text = "Products"
 
     def login_to_application(self, username, password):
         self.username_textField.fill(username)
         self.password_textField.fill(password)
         self.login_button.click()
 
+    def check_login_access(self):
+        # checks if the access is correct
+        expect(self.product_label).to_have_text(self.product_label_text)
+        get_product_text = self.product_label.text_content()
+        print(f"The product label text is {get_product_text}")
 
